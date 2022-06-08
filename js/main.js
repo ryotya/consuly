cee8fe50327e7a21e41efbe8c7eb11538292021b
Ryota Kakui <ryota6363@gmail.com>
2022-06-08 15:34:00 +0900

    Removed mobile view

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-$(".openbtn1").click(function () {
-    $(this).toggleClass('active');
-});
-
-$(function () {
-    $("mobilenav").hide();
-    $(".openbtn1").click(function () {
-        $("mobilenav").toggle(300);
-    });
-});
-
 $(function () {
     $(window).scroll(function () {
         const wHeight = $(window).height();
